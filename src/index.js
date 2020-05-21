6da6ff61988914d27a3c45f352ba86c457fd474b
Vishwa <vishwadharsan8@hmail.com>
2020-05-21 18:09:56 +0530

    created respective flow for directory/shop

--- src/index.js
+++ src/index.js
@@ -2,14 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import store from './redux/store';
+import { store, persister } from './redux/store';
 import './index.css';
 import App from './App';
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-      <App />
+      <PersistGate persistor={persister}>
+        <App />
+      </PersistGate>
     </BrowserRouter>
   </Provider>,
   document.getElementById('root')
